41579cf2a3518f8d3ed58b9744bff770053e716c
MerzlyakovaNatalya <94801202+MerzlyakovaNatalya@users.noreply.github.com>
2021-12-02 18:12:20 +0300

    lesson2

--- reactJs/myapp/src/App.js
+++ reactJs/myapp/src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
+import { Practice } from './lesson2/Practice';
 import Message from './Message';
-import img from "./images.png"
 
-const textMessage = 'Моё первое задание на курсе React';
+const textMessage = 'Моё второе задание на курсе React';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Message name={textMessage}></Message>
-        <p><img src={img} alt="lorem"></img></p>
       </header>
       <main>
+        <Practice></Practice>
       </main>
     </div>
   );
