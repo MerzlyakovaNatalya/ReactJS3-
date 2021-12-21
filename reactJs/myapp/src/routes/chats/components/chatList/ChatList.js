ff88c54c4012fb45f4ffdcbb7c8cfbd06342360f
MerzlyakovaNatalya <94801202+MerzlyakovaNatalya@users.noreply.github.com>
2021-12-21 17:37:50 +0300

    Update ChatList.js

--- reactJs/myapp/src/routes/chats/components/chatList/ChatList.js
+++ reactJs/myapp/src/routes/chats/components/chatList/ChatList.js
@@ -4,17 +4,17 @@ import { CHATS } from './../../../../helpers/mock'
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
 
-const ChatList = () => {
+const ChatList = ({list, onDelete}) => {
 
     return (
         <>
         <List
         sx={{marginRight: 5}}>
-            {CHATS.map(item => (
+            {list.map(item => (
                 <ListItem key={item.id}>
                     <Link to={`/chats/${item.id}`}>{item.name}</Link>
-                    {/*<Button to="/chats/:chatId" component={Link} color="inherit">{item.name} {`/chats/${item.id}`}
-                    </Button>*/}
+                    <Button onClick={onDelete} color="inherit">Delete
+                    </Button>
                 </ListItem>
             ))}
         </List>
