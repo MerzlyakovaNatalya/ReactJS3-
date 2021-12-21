c0cce12d7cbabcdc6f299e58e8398181b795a8ad
MerzlyakovaNatalya <94801202+MerzlyakovaNatalya@users.noreply.github.com>
2021-12-21 09:00:36 +0300

    Redux-persist

---
+++ reactJs/myapp/src/hocs/withChatMessages.js
@@ -0,0 +1,31 @@
+
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getChatMessagesById } from '../store/messages/selectors';
+import { hasChatById } from '../store/chats/selectors';
+import { createMessage } from '../store/messages/action';
+import { sendMessageWithThunk } from '../store/messages/action';
+import { USER_AUTHOR } from '../constants/userAutor';
+import { useRef } from 'react';
+
+export const withChatMessages = (Component) => {
+    return (props) => {
+        
+        const {chatId} = useParams(); 
+        const dispatch = useDispatch();
+        const messageList = useSelector(getChatMessagesById(chatId));
+        const hasChat = useSelector(hasChatById(chatId));
+
+
+        const onSubmitMessage = (text) => {
+            dispatch(sendMessageWithThunk(USER_AUTHOR, text, chatId))
+        };
+
+        return <Component
+           {...props}
+           messageList={messageList}
+           hasChat={hasChat}
+           onSubmitMessage={onSubmitMessage}
+           />
+    }
+}
