7f8cb83bc8d65ebae4c9817a8ad25d6026601038
MerzlyakovaNatalya <94801202+MerzlyakovaNatalya@users.noreply.github.com>
2021-12-28 17:14:23 +0300

    lesson9

--- reactJs/myapp/src/routes/Home.js
+++ reactJs/myapp/src/routes/Home.js
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-      <h1>Home</h1>  
+      <>
+      <h1>Home</h1> 
+      <div>
+      <Link to="/login">Sign In</Link>
+    </div>
+    <div>
+      <Link to="/signup">Sign Up</Link>
+    </div>
+      </> 
     )
 }
 
--- reactJs/myapp/src/store/chats/action.js
+++ reactJs/myapp/src/store/chats/action.js
@@ -1,3 +1,6 @@
+import {chatsRef} from "../../services/firebase";
+import {mapChatSnapshotToChat} from "../../helpers/index";
+
 export const ADD_CHAT = "ADD_CHAT"
 export const SET_CHATS = "SET_CHATS"
 export const REMOVE_CHAT = "REMOVE_CHAT"
@@ -16,3 +19,51 @@ export const removeChat = (chatId) => ({
     type: REMOVE_CHAT,
     payload: chatId,
 })
+
+export const removeChatWithThunk = (chatId) => () => {
+    chatsRef.child(chatId).remove(() => {
+    })
+}
+
+export const addChatWithThunk = (chat) => () => {
+    chatsRef.push(chat);
+}
+
+export const onTrackingAddChatWithThunk = (dispatch) => {
+    chatsRef.on('child_add', (snapshot) => {
+    dispatch(addChat(mapChatSnapshotToChat(snapshot)))
+    })
+}
+
+export const offTrackingAddChatWithThunk = () => {
+    chatsRef.off('child_added')
+}
+
+export const onTrackingRemoveChatWithThunk = (dispatch) => {
+    chatsRef.on('child_removed', (snapshot) => {
+        dispatch(removeChat(snapshot.key))
+    }) 
+}
+
+export const offTrackingRemoveChatWithThunk = () => {
+    chatsRef.off('child_removed')
+}
+
+/*export const ADD_CHAT = "ADD_CHAT"
+export const SET_CHATS = "SET_CHATS"
+export const REMOVE_CHAT = "REMOVE_CHAT"
+
+export const addChat = (chat) => ({
+    type: ADD_CHAT,
+    payload: chat,
+})
+
+export const setChats = (chats) => ({
+    type: SET_CHATS,
+    payload: chats,
+})
+
+export const removeChat = (chatId) => ({
+    type: REMOVE_CHAT,
+    payload: chatId,
+})*/
